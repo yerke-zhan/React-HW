535d917fe72e4e3e614c7f8c2d2f83a263731597
Yerke <yerkezhanm@gmail.com>
2025-04-08 20:45:12 +0500

    file added

--- Week29-HW/src/App.jsx
+++ Week29-HW/src/App.jsx
@@ -12,9 +12,13 @@ function App() {
   return (
    
     <>
-   <h1>Home Work useContext</h1>
-    {/* <FeedbackForm/>
-    <NewUser/> */}
+   
+     <h1>Validation</h1>
+     <FeedbackForm/>
+     <h1>HW-AXIOS</h1> 
+    <NewUser/>
+
+    <h1>Home Work useContext</h1>
     <LanguageProvider>
       <Test />
     </LanguageProvider>
